4e92abfd5a4b22c8083c8aef4a78ac8c50d9c50a
webfullstacks <webfullstack@yandex.ru>
2024-05-05 23:08:38 +0300

    fix

--- src/app/routers/private-routes.tsx
+++ src/app/routers/private-routes.tsx
@@ -1,32 +1,12 @@
-import { routes } from "@/shared/config";
-import { UserRoleService } from "@/shared/lib";
 import { UserRole } from "@/shared/types";
-import { Navigate, Outlet } from "react-router-dom";
-
-import { useAuth } from "../providers/auth-provider";
+import { Outlet } from "react-router-dom";
 
 interface PrivateRouterProps {
     roles?: UserRole[];
 }
 
 const PrivateRoute = ({ roles = [...Object.values(UserRole)] }: PrivateRouterProps) => {
-    // const { isAuth } = useAuth();
-
-    // const userRole = UserRoleService.getUserRoleFromLocalStorage();
-
-    // switch (isAuth()) {
-    //     // неавторизован
-    //     case false:
-    //         return <Navigate to={"/" + routes.yandexOAuthRedirectUri} replace />;
-
-    //     // авторизован
-    //     case true:
-    //         if (userRole && roles.includes(userRole)) {
-    //             return <Outlet />;
-    //         } else {
-    //             return <Navigate to={routes.lk} replace />;
-    //         }
-    // }
+    console.log(roles);
 
     return <Outlet />;
 };
